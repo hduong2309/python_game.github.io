32cbd3a2b41a747a6a4b9645d34426d1896c6867
hduong2309 <139541317+hduong2309@users.noreply.github.com>
2023-12-07 08:44:40 +0700

    Update space_shooter_ahihi.py

--- space_shooter_ahihi.py
+++ space_shooter_ahihi.py
@@ -1,10 +1,12 @@
 import pygame
 import random
 from pygame.locals import *
+from pygame import mixer;
 
 pygame.font.init()
 
 pygame.init()
+pygame.mixer.init()
 
 #Tao cua so
 screen_width = 800
@@ -17,6 +19,10 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255,0,0)
+#Back Ground music:
+bg_sound = pygame.mixer.Sound(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\background.wav')
+bg_sound.set_volume(0.25)
+bg_sound.play(-1)
 #Scale BG bang man hinh:    
 BG = pygame.transform.scale((pygame.image.load(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\earth-1756274_640.webp')),(screen_width,screen_height))    
 Player_ship =  pygame.image.load('E:\TUATUN\PYTHON GAME\Space_shooter\images\player.png')  
@@ -25,7 +31,7 @@ Enemy2 = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Space_shooter\images\enemy1_3
 Enemy3 = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Space_shooter\images\enemy2_1.png')  
 Enemy_Laser = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\bulletboss2.png')
 LASER = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Space_shooter\images\bullet1.png')
-LASER2 = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\bullet2.png')
+LASER2 = pygame.image.load(r'E:\TUATUN\PYTHON GAME\ddddddGame_ban_tau\images\bullet2.png')
 LASER3 = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\bullet3.png')
 LASER4 = pygame.image.load(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\bullet4.png')
 Enemy_image = [Enemy1,Enemy2,Enemy3]
@@ -45,6 +51,9 @@ title_font = pygame.font.SysFont("comicsans",60)
 score =0
 hscore = 0
 
+click_sound = mixer.Sound(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\click.wav')
+click_sound.set_volume(0.5)
+
 
 class Button:
     def __init__(self,x,y,image,scale):
@@ -59,7 +68,7 @@ class Button:
     def is_clicked(self):
         mouse_pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(mouse_pos):
-            return pygame.mouse.get_pressed()[0]
+            return pygame.mouse.get_pressed()[0]     
         else:
             return False
         
@@ -69,24 +78,11 @@ quit_buttom = Button(2*screen_width//3-50, screen_height//2-25,EXIT,0.5)
 key_bindings_buttom = Button(250,450,KEY_BINDINGS,1)
 back_buttom = Button(30,30,BACK,1)
 
-class Buttom:
-    def __init__(self,x,y,width,height,color,text):
-        self.rect = pygame.Rect(x,y,width,height)
-        self.color = color
-        self.text = text
-    def draw(self, surface):
-        pygame.draw.rect(surface,self.color,self.rect)
-        label = font.render(self.text, True, BLACK)
-        surface.blit(label,(self.rect.x + self.rect.width//2 - label.get_width()//2,
-                            self.rect.y + self.rect.height//2 - label.get_height()//2))
-    #Ham check xem buttom co duoc nhan hay khong?
-    def is_clicked(self):
-        mouse_pos = pygame.mouse.get_pos()
-        if self.rect.collidepoint(mouse_pos):
-            return pygame.mouse.get_pressed()[0]
-        else:
-            return False
-        
+# Ham kiem tra var cham:
+def collide(object1,object2):
+    offset_x = object2.x - object1.x
+    offset_y = object2.y - object1.y
+    return object1.mask.overlap(object2.mask,(offset_x,offset_y)) != None
 
 class Laser:
     def __init__(self,x,y,img):  
@@ -189,11 +185,7 @@ class Enemy(Ship):
             laser = Laser(self.x + self.get_width()/2-20, self.y, self.laser_image) #create laser o chinh giua ship cua player
             self.lasers.append(laser)
             self.cooldown_counter = 1 # Sau khi ban dat lai cooldown
-# Ham kiem tra var cham:
-def collide(object1,object2):
-    offset_x = object2.x - object1.x
-    offset_y = object2.y - object1.y
-    return object1.mask.overlap(object2.mask,(offset_x,offset_y)) != None
+
 
 
 def main_menu():
@@ -208,16 +200,20 @@ def main_menu():
         quit_buttom.draw(screen) # EXIT BUTTOM
         key_bindings_buttom.draw(screen) #KEY BINDING BUTTOM
         if start_buttom.is_clicked(): # Neu nhan nut START thi bat dau
+            click_sound.play()
             main()
         if quit_buttom.is_clicked(): # Neu nhan nut Exit thi thoat ra
+            click_sound.play()
             run = False
         if key_bindings_buttom.is_clicked():
+            click_sound.play()
             key_bindings()
         pygame.display.update()
     pygame.quit() 
 
 def key_bindings():
     run_key = True
+    click_sound.play()
     while run_key:
         screen.blit(BG,(0,0))
         for event in pygame.event.get():
@@ -235,6 +231,7 @@ def key_bindings():
         screen.blit(Space_txt,(320,450))
         back_buttom.draw(screen)
         if back_buttom.is_clicked():
+            click_sound.play()
             main_menu()
         pygame.display.update()
 def main():
@@ -282,8 +279,10 @@ def main():
             quit_buttom.draw(screen)
             score = 0
             if replay_buttom.is_clicked(): # Neu nhan nut REPLAY thi bat dau lai
+                click_sound.play()
                 main()
             if quit_buttom.is_clicked(): # Neu nhan nut Exit thi thoat ra
+                click_sound.play()
                 quit() 
             
              
@@ -305,12 +304,22 @@ def main():
             player.y += player_vel
         if keys[pygame.K_SPACE]:
             player.shoot()
+            #Laser Sound:
+            laser_sound = mixer.Sound(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\laser.wav')
+            laser_sound.set_volume(0.4)
+            laser_sound.play()
         
         draw_window()
         #Dieu Kien Thua Game:
         if lives <=0 or player.health <=0:
             gameOver = True
             player.score = 0
+            #Game Over Sound:
+            game_over_sound = mixer.Sound(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\game_over.wav')
+            game_over_sound.set_volume(0.6)
+            game_over_sound.play()
+            lives = 3
+            player.health = 100
          
         if len(enemies) == 0:
             #Increase level and number of enemies:
@@ -327,10 +336,14 @@ def main():
             enemy.move(enemy_vel)
             enemy.move_laser(enemy_laser_vel,player)
             #enemy ban dan:
-            if random.randrange(0,120*4) == 1:
+            if random.randrange(0,120*6) == 1:
                 enemy.shoot()
             if collide(enemy, player):#Neu Player var cham voi enemy thi -10hp va remove enemy
                 player.health -= 10
+                #Explosion Sound:
+                explosion_sound = mixer.Sound(r'E:\TUATUN\PYTHON GAME\Game_ban_tau\images\explosion.wav')
+                explosion_sound.set_volume(0.4)
+                explosion_sound.play()
                 enemies.remove(enemy)
             elif enemy.y + enemy.get_height() > screen_height:
                 lives -= 1 
@@ -342,6 +355,13 @@ main_menu()
   
     
     
+        
+        
+        
+        
+        
+        
+        
         
         
         
